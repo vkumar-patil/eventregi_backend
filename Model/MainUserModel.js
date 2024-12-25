d9fa72c08e87df3ad47171ebf28981f1358ec31e
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2024-12-25 12:00:47 +0530

    update

--- Model/MainUserModel.js
+++ Model/MainUserModel.js
@@ -3,5 +3,6 @@ const MainSchema = mongoose.Schema({
   username: { type: String, require: true },
   email: { type: String, require: true },
   password: { type: String, require: true },
+  Admin: { type: Boolean, default: false },
 });
-module.exports=mongoose.model("mainusers",MainSchema)
+module.exports = mongoose.model("mainusers", MainSchema);
